f2ceb2b22a4bf2910d8d7c5a00d51148b4763ce9
fzrsahi <fazrul.anugrah17@gmail.com>
2022-08-22 23:39:14 +0800

    add skill wrapper

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-const sayHalo =  () => {
-  console.log("halo");
-};
+const midList = document.getElementsByClassName("mid-list");
+midList[1].innerHTML = "Skill";
+
+const scroll = document.querySelector(".scroll");
+scroll.style.marginTop ="200px";
+
+const linkedIn = document.getElementsByClassName("fa-linkedin");
+linkedIn[0].href = "https://www.linkedin.com/in/fazrul-sahi-b26219236/";
+
+const instagram = document.querySelector(".fa-instagram");
+instagram.href = "https://www.instagram.com/fzrsahi/";
+
+//nantinya buat thema light dan darkmode
+
+// const header = document.querySelector("header");
+// const headerLeft = header.querySelector(".header-left");
+
+// headerLeft.addEventListener("click" ,() => {
+//   headerLeft.style.backgroundColor ="white";
+//   headerLeft.style.color = "black";
+// });
+
